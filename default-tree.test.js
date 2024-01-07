a047d5b4939bba0fcbf8bf3ca4816d3d4e695c21
gustcomer <gustcomer@gmail.com>
2024-01-07 16:39:27 -0300

    test: refactoring the tests with beforeAll

--- default-tree.test.js
+++ default-tree.test.js
@@ -1,13 +1,8 @@
 const {Node, Tree} = require('./default-tree.js')
 
-/*
-test('Should create an empty default-tree', function () {
-  t = new Tree()
-});
-*/
+let t;
 
-test('Should create a simple tree', function () {
-
+beforeEach(() => {
   const nA = new Node('A')
   const nB = new Node('B')
   const nC = new Node('C')
@@ -19,25 +14,16 @@ test('Should create a simple tree', function () {
   nA.addChild(nD)
   nB.addChild(nE)
 
-  const t = new Tree(nA)
+  t = new Tree(nA)
+});
+
+test('Should create a simple tree', function () {
 
   expect(t).toBeDefined()
 });
 
 test('Should transform to string a simple tree', function () {
 
-  const nA = new Node('A')
-  const nB = new Node('B')
-  const nC = new Node('C')
-  const nD = new Node('D')
-  const nE = new Node('E')
-
-  nA.addChild(nB)
-  nA.addChild(nC)
-  nA.addChild(nD)
-  nB.addChild(nE)
-
-  const t = new Tree(nA)
   const text = t.toString()
 
   expect(text).toBe('A B E C D')
