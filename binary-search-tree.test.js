d10987488195b453b5f239d12677794bf276aa44
gustcomer <gustcomer@gmail.com>
2024-01-15 21:47:27 -0300

    test: Should create a simple Binary Search Tree

---
+++ binary-search-tree.test.js
@@ -0,0 +1,15 @@
+const {Node, BinarySearchTree} = require('./binary-search-tree.js')
+
+let bst;
+let n5;
+
+beforeEach(() => {
+  const n5 = new Node(5)
+
+  bst = new BinarySearchTree()
+});
+
+test('Should create a simple Binary Search Tree', function () {
+
+  expect(bst).toBeDefined()
+});
