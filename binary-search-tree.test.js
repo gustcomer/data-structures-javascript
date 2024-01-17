ccafa1650dc10ccf5d712fef4c7a30bd6f0adb5f
gustcomer <gustcomer@gmail.com>
2024-01-17 06:16:42 -0300

    test: add searchNode()

--- binary-search-tree.test.js
+++ binary-search-tree.test.js
@@ -36,10 +36,19 @@ test('Should create a simple Binary Search Tree', function () {
   expect(bst).toBeDefined()
 });
 
-test('Should return string of Node', () => {
+test('Should transform Node into string', () => {
   expect(n6.toString()).toBe('6 10 12 ')
 })
 
-test('Should return string of tree', () => {
+test('Should transform Tree into Node', () => {
   expect(bst.toString()).toBe('5 4 0 2 6 10 12')
+})
+
+test('Should return true if the key matches some Node or subNode value', () => {
+  expect(n5.searchNode(4)).toBe(true)
+  expect(n5.searchNode(5)).toBe(true)
+  expect(n5.searchNode(6)).toBe(true)
+  expect(n5.searchNode(7)).toBe(false)
+  expect(n5.searchNode(200)).toBe(false)
+  expect(n5.searchNode(12)).toBe(true)
 })
